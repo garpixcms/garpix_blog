f40642b6c12cea8533249285cd048991c106b778
lMaCkRagel <jmaklargenj@gmail.com>
2021-07-20 17:17:37 +0300

    KONTINENT-292

--- backend/garpix_blog/models/post_video.py
+++ backend/garpix_blog/models/post_video.py
@@ -8,11 +8,8 @@ from garpix_blog.utils import get_file_path
 PostVideosMixin = import_string(settings.GARPIX_BLOG_POST_VIDEO_MIXIN)
 
 
-class PostVideos(models.Model):
-    title = models.CharField(max_length=250, verbose_name='Название')
+class PostVideos(PostVideosMixin, models.Model):
     video = models.FileField(verbose_name="Видео", blank=True, null=True, upload_to=get_file_path)
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
-    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
     post = models.ForeignKey(PostPage, on_delete=models.SET_NULL, related_name='videos', blank=True, null=True)
 
     def __str__(self):
