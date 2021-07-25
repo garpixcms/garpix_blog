b07355cb0cf12c372675435a4b165fdd9b80651c
crusat <crusat@yandex.ru>
2021-07-25 14:41:03 +0300

    Теперь менеджер и миксин внутри модуля

--- backend/garpix_blog/setup.py
+++ backend/garpix_blog/setup.py
@@ -9,7 +9,7 @@ with open(path.join(settings.BASE_DIR, '..', 'README.md'), encoding='utf-8') as 
 
 setup(
     name='garpix_blog',
-    version='1.0.0',
+    version='1.1.0',
     description='',
     long_description=long_description,
     url='https://github.com/garpixcms/garpix_blog',
